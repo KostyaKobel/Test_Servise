e1bfdd1e862a16858d6b80638b0a96eff54ec75f
Kostyantyn <kostp62020@gmail.com>
2021-08-06 11:33:38 +0300

    writed link model tests

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Link do
+  context 'Model' do
+    it 'is valid if it has an original URL and a short url' do
+      link = Link.new(
+        original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
+        short_url: 'https://www.favoritewebsite.com/'
+      )
+      expect(link.valid?).to be(true)
+    end
+
+    it 'is invalid if does not have short url' do
+      link = Link.new(
+        original_url: 'https://www.favoritewebsite.com/articles/how-to-cook'
+      )
+      expect(link.valid?).to be(false)
+    end
+
+    it 'is invalid if does not have original url' do
+      link = Link.new(
+        short_url: 'https://www.favoritewebsite.com/'
+      )
+      expect(link.valid?).to be(false)
+    end
+
+    it 'is invalid if does not have original url' do
+      link = Link.new(
+        original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
+        short_url: 'https://www.favoritewebsite.com/'
+      )
+      link.save
+
+      link2 = Link.new(
+        original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
+        short_url: 'https://www.favoritewebsite.com/'
+      )
+
+      expect(link2.valid?).to be(false)
+    end
+  end
+
+end
