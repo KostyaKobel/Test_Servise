38fba054986fcaaf2eea06cf67b0e16e3e84d8b5
Kostyantyn <kostp62020@gmail.com>
2021-08-06 11:38:08 +0300

    removed don't need routes and add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'links/index'
-  get 'links/show'
-  get 'links/new'
-  get 'links/create'
-  get 'links/edit'
-  get 'links/destroy'
+  root to: 'home#index'
 
   post '/links' => 'links#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
