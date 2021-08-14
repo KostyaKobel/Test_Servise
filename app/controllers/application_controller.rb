3f774110f41904c4df984f664220f281ff86b8d9
Kostyantyn <kostp62020@gmail.com>
2021-08-14 11:03:05 +0300

    add basic authenticate for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include HttpAuthConcern
+  http_basic_authenticate_with name: 'admin', password: 'password'
 end
