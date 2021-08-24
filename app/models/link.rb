1b081c34ef0ef4dbbd9bd9a7be68828af89367e4
Kostyantyn <kostp62020@gmail.com>
2021-08-24 22:00:50 +0300

    add some methods to generate and create short_url and add link_destroy_greater_than_two methods

--- app/models/link.rb
+++ app/models/link.rb
@@ -19,43 +19,41 @@ class Link < ApplicationRecord
     "http://localhost:3000/#{short_url}"
   end
 
-  def link_destroy_greater_than_two(*links)
-    binding.pry
-    links.each do |link|
-      months = 2678400
-      if Date.parse(link.last_date_visit_link) > 2.months then
-        link.short_url.destroy
-      end
-    end
+  def self.link_destroy_greater_than_two
+    Link.where('last_date_visit_link < ?', 2.months.ago).delete_all
   end
 
   def self.ransackable_attributes(auth_object = nil)
     ["short_url", "visit_link_count"]
   end
 
-  # # auto slug generation
-  # before_validation :generate_short_url
-  #
-  # def generate_short_url
-  #   self.short_url = SecureRandom.uuid[0..5] if self.short_url.nil? || self.short_url.empty?
-  #   true
-  # end
-  #
-  # # fast access to the shortened link
-  # def short
-  #   Rails.application.routes.url_helpers.short_url(short_url: self.short_url)
-  # end
-  #
-  # # the API
-  # def self.shorten(original_url, short_url = '')
-  #   link = Link.where(original_url: original_url, short_url: short_url).first
-  #   return link.short if link
-  #
-  #   link = Link.new(original_url: original_url, short_url: short_url)
-  #   return link.short if link.save
-  #
-  #   Link.shorten(original_url, short_url + SecureRandom.uuid[0..2])
-  # end
+  def register_visit
+    update(visit_link_count: visit_link_count + 1, last_date_visit_link: Time.now)
+  end
+
+  # auto slug generation
+  before_validation :generate_short_url
+
+  def generate_short_url
+    self.short_url = SecureRandom.uuid[0..5] if self.short_url.nil? || self.short_url.empty?
+    true
+  end
+
+  # fast access to the shortened link
+  def short
+    Rails.application.routes.url_helpers.short_url(short_url: self.short_url)
+  end
+
+  # the API
+  def self.shorten(original_url, short_url = '')
+    link = Link.where(original_url: original_url, short_url: short_url).first
+    return link.short if link
+
+    link = Link.new(original_url: original_url, short_url: short_url)
+    return link.short if link.save
+
+    Link.shorten(original_url, short_url + SecureRandom.uuid[0..2])
+  end
 
 
 end
