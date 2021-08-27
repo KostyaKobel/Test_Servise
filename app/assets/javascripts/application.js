8a1b363913dccd1d9ad28206c3cb2f19eef9ef45
Kostyantyn <kostp62020@gmail.com>
2021-08-27 20:50:15 +0300

    remove doesn't need code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,14 +12,3 @@
 //
 //= require turbolinks
 //= require_tree .
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap/dist/js/bootstrap"
-import "bootstrap/dist/css/bootstrap"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
