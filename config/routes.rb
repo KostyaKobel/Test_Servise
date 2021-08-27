c7896beb726b6108da57644d44105a1d68b892c2
Kostyantyn <kostp62020@gmail.com>
2021-08-27 20:55:29 +0300

    add named link_destroy route to admin links link_destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'links#index'
+  # get '/s/:slug', to: 'links#show', as: :short
   namespace :admin do
     resources :links, only: [:index, :show, :destroy]
   end
+  delete 'link_destroy' => 'admin/links#link_destroy'
   resources :links
 end
