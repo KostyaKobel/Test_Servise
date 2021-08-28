d4efea0013f3e0e4b76bc1219f27fef02334a216
Kostyantyn <kostp62020@gmail.com>
2021-08-28 14:26:34 +0300

    add link factories

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :link do
-    
+    short_url { Faker::Internet.url(host: 'localh') }
+    original_url { Faker::Internet.url(host: 'localhost', path: '/fake_test_path', scheme: 'https') }
+    generate_link_password { Faker::Internet.password(min_length: 8, max_length: 16) }
+    last_date_visit_link { 3.months.ago }
   end
 end
