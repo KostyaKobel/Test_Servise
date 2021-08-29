4da16898f470e314445f508d04164d4ce9faf2af
Kostyantyn <kostp62020@gmail.com>
2021-08-29 19:58:53 +0300

    add tests to links views index show new edit

--- spec/views/links/index.html.erb_spec.rb
+++ spec/views/links/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "links/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'links/index.html.slim', type: :view do
+  context 'when template rendered' do
+    it 'infers the controller path' do
+      expect(controller.controller_path).to eq('links')
+    end
+
+    it 'infers the controller action' do
+      expect(controller.request.path_parameters[:action]).to eq('index')
+    end
+  end
 end
--- spec/views/links/new.html.erb_spec.rb
+++ spec/views/links/new.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "links/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'links/new.html.slim', type: :view do
+  context 'when template rendered' do
+    it 'infers the controller path' do
+      expect(controller.controller_path).to eq('links')
+    end
+
+    it 'infers the controller action' do
+      expect(controller.request.path_parameters[:action]).to eq('new')
+    end
+  end
 end
