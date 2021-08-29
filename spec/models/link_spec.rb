c8b8f24a98c9b8f26501ade8e9f16b953f24fbdc
Kostyantyn <kostp62020@gmail.com>
2021-08-29 17:33:12 +0300

    add links model tests

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
     context 'when valid' do
 
       let(:test_link) { FactoryBot.create(:link) }
+
       it 'create link' do
          expect(test_link).to be_valid
       end
@@ -37,59 +38,24 @@ require 'rails_helper'
       end
     end
 
-    # context 'when Method' do
-    #   let(:link) { Link.create(:link) }
-    #   it 'should destroy links which greater than two months' do
-    #     Link.link_destroy_greater_than_two.should delete_all(:link)
-    #   end
-    #
-    # end
+    context 'when Method' do
+      let(:link) { Link.create(short_url: 'localo',
+                                original_url: 'localhost/fake_test_path',
+                                generate_link_password: '12345678122314',
+                                last_date_visit_link: 3.months.ago) }
+      let(:destroy_link) { Link.create( short_url: 'localo',
+                                original_url: 'localhost/fake_test_path',
+                                generate_link_password: '12345678122314') }
+
+      it 'doesnt have last_date_visit_link greater than two months' do
+        Link.link_destroy_greater_than_two
+        expect(link).not_to be(true)
+      end
+
+
+      it 'doesnt destroy short url' do
+        Link.update(short_url: nil)
+        expect(destroy_link).not_to be(true)
+      end
+    end
   end
-# RSpec.describe Link do
-#   context 'Model' do
-#     it 'is valid if it has an original URL and a short url' do
-#       link = Link.new(
-#         original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
-#         short_url: 'https://www.favoritewebsite.com/'
-#       )
-#       expect(link.valid?).to be(true)
-#     end
-#
-#     it 'is invalid if the URL is not formatted properly' do
-#       link = Link.new(
-#         original_url: 'asdsdgsdg',
-#         short_url: 'https://www.favoritewebsite.com/'
-#       )
-#       expect(link.valid?).to be(false)
-#     end
-#
-#     it 'is invalid if does not have short url' do
-#       link = Link.new(
-#         original_url: 'https://www.favoritewebsite.com/articles/how-to-cook'
-#       )
-#       expect(link.valid?).to be(false)
-#     end
-#
-#     it 'is invalid if does not have original url' do
-#       link = Link.new(
-#         short_url: 'https://www.favoritewebsite.com/'
-#       )
-#       expect(link.valid?).to be(false)
-#     end
-#
-#     it 'is invalid if does not have original url' do
-#       link = Link.new(
-#         original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
-#         short_url: 'https://www.favoritewebsite.com/'
-#       )
-#       link.save
-#
-#       link2 = Link.new(
-#         original_url: 'https://www.favoritewebsite.com/articles/how-to-cook',
-#         short_url: 'https://www.favoritewebsite.com/'
-#       )
-#
-#       expect(link2.valid?).to be(false)
-#     end
-#   end
-# end
