d517a4037d3cff28bafadf95b2557fc48fb090f5
Kostyantyn <kostp62020@gmail.com>
2021-08-29 19:57:12 +0300

    changed original_url value

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :link do
     short_url { Faker::Internet.url(host: 'localh') }
-    original_url { Faker::Internet.url(host: 'localhost', path: '/fake_test_path', scheme: 'https') }
+    original_url { Faker::Internet.url(host: 'www.grammar.cl', path: '/english/how-much-how-many.htm', scheme: 'https://') }
     generate_link_password { Faker::Internet.password(min_length: 8, max_length: 16) }
-    last_date_visit_link { 3.months.ago }
   end
 end
